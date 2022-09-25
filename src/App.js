27f1c984fa31af6ddbf172629e2ed67c7f9983f8
yash <yashwable12@gmail.com>
2022-09-25 19:47:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
